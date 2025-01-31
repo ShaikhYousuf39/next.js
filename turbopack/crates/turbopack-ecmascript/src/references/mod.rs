0fa619b4a36527b90b710dd38e83d8dc0a28c1c4
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-01-31 14:53:20 +0100

    Cleanup ConstantConditionValue

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -960,7 +960,7 @@ pub(crate) async fn analyse_ecmascript_module_internal(
                         ($expr:expr) => {
                             if !condition_has_side_effects {
                                 analysis.add_code_gen(ConstantConditionCodeGen::new(
-                                    Value::new($expr),
+                                    $expr,
                                     condition_ast_path.to_vec().into(),
                                 ));
                             }
