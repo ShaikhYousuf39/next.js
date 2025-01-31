2f12d267db87b630f6921956bc620a6cb3210133
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-01-31 14:00:41 +0100

    wip

--- turbopack/crates/turbopack-ecmascript/src/references/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/mod.rs
@@ -168,7 +168,6 @@ pub struct AnalyzeEcmascriptModuleResultBuilder {
     async_module: ResolvedVc<OptionAsyncModule>,
     successful: bool,
     source_map: Option<ResolvedVc<OptionSourceMap>>,
-    bindings: Vec<EsmBinding>,
 }
 
 impl AnalyzeEcmascriptModuleResultBuilder {
@@ -183,7 +182,6 @@ impl AnalyzeEcmascriptModuleResultBuilder {
             async_module: ResolvedVc::cell(None),
             successful: false,
             source_map: None,
-            bindings: Vec::new(),
         }
     }
 
